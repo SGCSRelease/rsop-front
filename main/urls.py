f8b1826ae607bb650cdf10b6efe65127091deea6
Namgyu Ho <itsnamgyu@gmail.com>
2018-07-19 01:03:38 +0900

    remove index from url

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from main import views
 
 urlpatterns = [
-    path('index', views.index, name='index'),
+    path('', views.index, name='index'),
     path('rsop2018', views.index, name='rsop'),
     path('rsop2018/projects/<int:pk>', views.project, name='rsop-projects'),
     path('rsop2018/projects/feedback/<int:pk>', views.project_feedback, name='rsop-project-feedback'),
