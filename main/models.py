af4706e02e59dfe6b8d69e60f056dbc4f4b4e584
Namgyu Ho <itsnamgyu@gmail.com>
2018-07-18 17:42:06 +0900

    fix models (relatioship issue) and add dummy data

--- main/models.py
+++ main/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 
 
-class Feedback(models.Model):
-    creation_time = models.DateTimeField()
-    name = models.CharField(max_length=256)
-    content = models.TextField()
-
-
 class Project(models.Model):
     name = models.CharField(max_length=256)
     summary = models.CharField(max_length=1024)
@@ -17,4 +11,15 @@ class Project(models.Model):
     color_g = models.FloatField()
     color_b = models.FloatField()
 
-    feedback = models.ForeignKey(Feedback, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
+
+class Feedback(models.Model):
+    creation_time = models.DateTimeField()
+    name = models.CharField(max_length=256)
+    content = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'Feedback [{},{}]'.format(self.project, self.name)
